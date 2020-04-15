3522b78d4856ed77592ac5fc162be6ccbe397240
hyeonseo <hre2003@naver.com>
2020-04-16 02:56:47 +0900

    commit 1

---
+++ 1210_1218/src/Train/TrainTest.java
@@ -0,0 +1,35 @@
+package Train;
+
+public class TrainTest {
+	Booking booking;
+	Search search;
+	
+	public static void main(String[] args){
+
+		TrainTest main = new TrainTest();
+		
+		main.search = new Search(); // 로그인창 보이기
+        main.search.setMain(main);
+        main.search.SearchFrame();
+        
+        main.booking = new Booking(main.search); // 로그인창 보이기
+        main.booking.setMain(main);
+        main.booking.BookingFrame(main.search);
+        main.booking.dispose();
+        
+	}
+	
+	public void showBooking() {
+		this.search.setVisible(false);
+        this.booking.setVisible(true); 
+	}
+	public void showSearch() {
+		this.booking.setVisible(false);
+		this.search.setVisible(true);
+	}
+	
+	public void End() {
+		System.exit(0);
+	}
+
+}
---
+++ 1210_1218/src/Train/test.java
@@ -0,0 +1,16 @@
+package Train;
+
+public class test{
+	public static void main(String[] args) {
+		String month="04";
+		String day="16";
+		for(int k=1;k<=7;k++) {
+			for (int i=1;i<=7;i++) {
+				if(i==k)continue;
+				for(int j=1; j<=9; j++) {
+					System.out.println("insert into seat (train) values ("+k+i+month+day+j+");");
+				}
+			}
+		}
+	}
+}
